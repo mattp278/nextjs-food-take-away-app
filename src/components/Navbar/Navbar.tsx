f6534305d4b17044c371f5f50445d505bdab0767
Matthew Powell <matt.powell2784@gmail.com>
2023-03-27 21:46:37 +0100

    nav bar react-icons bundle size fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { TfiMenu } from 'react-icons/tfi'
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 w-screen h-[3rem] text-lg flex flex-row items-center justify-between p-4 text-primaryRed bg-whiteFloral z-10">
       {/* <Link href="/signup/sign-up-page">SIGN UP</Link>
       <Link href="/signup/sign-up-page">FOOD MENU</Link> */}
-      <GiHamburgerMenu size="2em" />
+      <TfiMenu size="2em" />
       <div className="relative w-[12rem] h-[3rem] flex items-center justify-center">
         <Image
-          src="/curry_club_logo.svg"
+          src="/curry_club_logo.png"
           fill
-          style={{ objectFit: 'fill' }}
-          alt="Logo"
+          style={{ objectFit: 'contain' }}
+          alt="Indian Platter"
         />
       </div>
     </nav>
