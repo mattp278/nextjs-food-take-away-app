5e0ebfd233f004d45cfb57daba8700d1410cdcd2
Matthew Powell <matt.powell2784@gmail.com>
2023-03-28 18:27:36 +0100

    hero logo updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export const Hero = () => {
   return (
-    <main className="relative w-screen min-h-screen min-w-[280px]">
+    <main className="relative w-screen h-[100px] min-h-screen min-w-[280px]">
       <div className="absolute w-full h-full z-0 ">
         <Image
           src="/curry_platter_bg.jpg"
@@ -11,16 +11,14 @@ export const Hero = () => {
           alt="Indian Platter"
         />
       </div>
-      <section className="relative flex w-full min-h-screen flex-col items-center justify-center py-6">
-        <article className="relative flex w-full h-1/6 min-w-[280px] min-h-[130px] rounded-xl bg-whiteFloral">
-          <div className="relative flex w-full h-1/6 min-w-[280px] min-h-[130px] rounded-xl p-20">
-            <Image
-              src="/curry_club_logo.png"
-              fill
-              style={{ objectFit: 'scale-down' }}
-              alt="Curry Lcub Logo"
-            />
-          </div>
+      <section className="relative flex min-h-screen flex-col items-center justify-center">
+        <article className="relative flex justify-center h-1/6 w-5/6 md:w-4/6 lg:w-1/2 min-w-[280px] min-h-[100px] md:min-h-[150px] rounded-xl bg-primaryRed p-12">
+          <Image
+            src="/curry_club_gold.png"
+            fill
+            style={{ objectFit: 'contain' }}
+            alt="Curry Club Logo"
+          />
         </article>
       </section>
     </main>
