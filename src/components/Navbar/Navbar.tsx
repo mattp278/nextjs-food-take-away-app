0b373d2710284e19bd586611f42c26486f34deb9
Matthew Powell <matt.powell2784@gmail.com>
2023-03-29 17:07:37 +0100

    nav bar logo home link added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { BiRestaurant } from 'react-icons/bi'
 import { AiFillHome } from 'react-icons/ai'
 import { MdOutlineReceiptLong } from 'react-icons/md'
@@ -13,14 +14,16 @@ export const Navbar = () => {
       }}
     >
       <nav className="fixed top-0 w-screen min-w-[280px] h-[5rem] text-lg flex flex-col items-center justify-center bg-primaryRed text-whiteFloral z-10">
-        <div className="relative w-[14rem] min-h-[2.2rem] flex items-center justify-center">
-          <Image
-            src="/curry_club_gold_sm.png"
-            fill
-            style={{ objectFit: 'contain' }}
-            alt="Indian Platter"
-          />
-        </div>
+        <Link href="/">
+          <div className="relative w-[14rem] min-h-[2.2rem] flex items-center justify-center">
+            <Image
+              src="/curry_club_gold_sm.png"
+              fill
+              style={{ objectFit: 'contain' }}
+              alt="Indian Platter"
+            />
+          </div>
+        </Link>
         <div className="w-5/6 flex flex-row justify-between gap-4">
           <BiRestaurant />
           <AiFillHome />
