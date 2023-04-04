abb6b0ebba953f7184c6a112710e7b5a97870f23
Matthew Powell <matt.powell2784@gmail.com>
2023-04-04 17:46:37 +0100

    onClick added to button ui compoment

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,13 +2,19 @@ interface buttonProps {
   text: string
   type: 'button' | 'submit' | 'reset'
   optionalClassNames?: string
+  onClick?: () => void
 }
 
-export const Button = ({ text, type, optionalClassNames }: buttonProps) => {
+export const Button = ({
+  text,
+  type,
+  optionalClassNames,
+  onClick,
+}: buttonProps) => {
   const buttonClassNames = `bg-primaryRed p-3 text-whiteFloral ${optionalClassNames}`
 
   return (
-    <button type={type} className={buttonClassNames}>
+    <button type={type} className={buttonClassNames} onClick={onClick}>
       {text}
     </button>
   )
