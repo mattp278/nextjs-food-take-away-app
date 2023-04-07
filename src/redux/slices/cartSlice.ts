bb5319b480e4e4aa6214a5e6f2d88afbc117f7c1
Matthew Powell <matt.powell2784@gmail.com>
2023-04-07 11:43:04 +0100

    cart slice total price added

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,14 +1,17 @@
 import { createSlice, createAsyncThunk, AnyAction } from '@reduxjs/toolkit'
 import { AppState } from '../store/store'
 import { CartMenuItemInterface } from '@/ts/interfaces'
+import { stat } from 'fs'
 
 export interface CartState {
   numOfOrderItems: number
+  totalPrice: number
   order: CartMenuItemInterface[]
 }
 
 const initialState: CartState = {
   numOfOrderItems: 0,
+  totalPrice: 0,
   order: [],
 }
 
@@ -25,6 +28,8 @@ export const cartSlice = createSlice({
 
       if (existingItemIndex !== -1) {
         state.order[existingItemIndex].quantity += quantity
+        state.numOfOrderItems += quantity
+        state.totalPrice += payload.price * quantity
         return
       }
 
@@ -32,6 +37,7 @@ export const cartSlice = createSlice({
       state.order.push(item)
 
       state.numOfOrderItems += quantity
+      state.totalPrice += payload.price * quantity
     },
   },
   extraReducers: (builder) => {},
