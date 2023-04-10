84e2e75961cef61cdccfc18179d47ab6c050e87c
Matthew Powell <matt.powell2784@gmail.com>
2023-04-10 19:03:41 +0100

    foodId page with get static paths function added

--- src/pages/pages/food-menu/[...foodId].tsx
+++ src/pages/pages/food-menu/[...foodId].tsx
@@ -1,13 +1,34 @@
-import { GetServerSideProps } from 'next'
+import { apiCall } from '@/utils/apiUtil'
 
-const [...foodId] = () => {
-  return <div>Enter</div>
+export default function FoodItemPage({ foodItem }) {
+  const { name } = foodItem
+  return <div>{name}</div>
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export async function getStaticPaths() {
+  const foodItemRequest = await apiCall({
+    httpMethod: 'GET',
+    route: 'http://localhost:3000/api/v1/food-item/food-item',
+  })
+
+  const paths = foodItemRequest.data.map((foodItem) => {
+    return { params: { foodId: [foodItem.id.toString()] } }
+  })
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context) {
+  const foodId = context.params.foodId
+  const foodItemRequest = await apiCall({
+    httpMethod: 'GET',
+    route: `http://localhost:3000/api/v1/food-item/${foodId}`,
+  })
+
+  const foodItem = foodItemRequest.data.foodItem
+
   return {
-    props: {},
+    props: { foodItem },
+    revalidate: 60,
   }
 }
-
-export default [...foodId]
