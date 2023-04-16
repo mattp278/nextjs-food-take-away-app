459a0bc43c9a4dd0272097dc5af50d0bdc1b736c
Matthew Powell <matt.powell2784@gmail.com>
2023-04-16 08:55:29 +0100

    react-icons uninstalled

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { IconContext } from 'react-icons'
 import { wrapper } from '../redux/store/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
@@ -13,16 +12,9 @@ const persistor = persistStore(store)
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <IconContext.Provider
-        value={{
-          size: '3rem',
-          className: 'fill-primaryRed',
-        }}
-      >
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </IconContext.Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
