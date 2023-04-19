5061cde28187b264de3899e0e29946c6b4936ddb
Matthew Powell <matt.powell2784@gmail.com>
2023-04-19 14:41:02 +0100

    next auth initial setup completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { wrapper } from '../redux/store/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
-import { makeStore, persistConfig } from '../redux/store/store'
+import { makeStore } from '../redux/store/store'
 import { persistStore } from 'reduxjs-toolkit-persist'
+import { SessionProvider } from 'next-auth/react'
 
 const store = makeStore()
 const persistor = persistStore(store)
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ session, Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </SessionProvider>
   )
 }
