abec65813b310bf073364fff405ef53eddd4aeb0
Matthew Powell <matt.powell2784@gmail.com>
2023-04-26 20:01:56 +0100

    get auth user moved to nav bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { NavItems } from './NavItems'
+import { useAppDispatch } from '@/redux/store/reduxHooks'
+import { getAuthUser } from '@/redux/slices/userSlice'
 
 export const Navbar = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const getUser = async () => {
+      await dispatch(getAuthUser())
+    }
+    getUser()
+  }, [dispatch])
+
   return (
     <nav className="fixed top-0 w-screen min-w-[280px] h-[5rem] text-lg flex flex-col md:flex-row items-center justify-center bg-primaryRed text-whiteFloral z-50">
       <Link href="/">
--- src/pages/pages/food-menu/food-menu.tsx
+++ src/pages/pages/food-menu/food-menu.tsx
@@ -1,22 +1,9 @@
-import { useEffect } from 'react'
 import { Main, Navbar, FoodMenu } from '@/components'
 import { apiCall } from '@/utils/apiUtil'
 import { TSFoodMenuItems } from '@/ts/interfaces'
 import { OrderFooter } from '@/components/OrderFooter/OrderFooter'
-import { useAppDispatch } from '@/redux/store/reduxHooks'
-import { getAuthUser } from '@/redux/slices/userSlice'
 
 export default function FoodMenuPage({ menuItems }: TSFoodMenuItems) {
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    const getUser = async () => {
-      const user = await dispatch(getAuthUser())
-      console.log('user', user)
-    }
-    getUser()
-  }, [dispatch])
-
   return (
     <>
       <title>Curry Club</title>
