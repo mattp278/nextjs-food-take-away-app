85c749d35dd0c708a9e75d847f450e5fc797eafa
Matthew Powell <matt.powell2784@gmail.com>
2023-04-26 10:53:58 +0100

    callback url added to withAuth HOC

--- src/pages/withAuth.tsx
+++ src/pages/withAuth.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
-
 import { ComponentType } from 'react'
 
 export default function withAuth<T>(Component: ComponentType<T>) {
@@ -13,7 +12,8 @@ export default function withAuth<T>(Component: ComponentType<T>) {
     }
 
     if (!session) {
-      router.replace('/api/auth/signin')
+      const callbackUrl = encodeURIComponent(router.asPath)
+      router.replace(`/api/auth/signin?callbackUrl=${callbackUrl}`)
       return null
     }
 
