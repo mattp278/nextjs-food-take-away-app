f56276454140375200c62c3839565545eb515bbe
Matthew Powell <matt.powell2784@gmail.com>
2023-04-27 18:05:18 +0100

    custom signin page email provider logic updated

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -8,12 +8,62 @@ import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../api/auth/[...nextauth]'
 import { Main, Navbar, Button } from '@/components'
 import { SmallShopAlt } from 'iconoir-react'
-import { SignInProviders } from './signInProviders'
 
 export default function SignIn({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  //
+  const [email, setEmail] = useState('')
+
+  const handleEmailSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await signIn('email', {
+      email,
+      callbackUrl: '/pages/food-menu/food-menu',
+    })
+  }
+
+  const emailProvider = Object.values(providers).map((provider) => {
+    if (provider.name === 'Email') {
+      return (
+        <div key={provider.name}>
+          <form
+            onSubmit={handleEmailSignIn}
+            className="flex flex-col items-center"
+          >
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email address"
+              className="w-full p-2 rounded border"
+            />
+            <Button
+              type="submit"
+              optionalClassNames="w-full p-2 rounded-lg my-2"
+              text={`Sign in with ${provider.name}`}
+            ></Button>
+          </form>
+        </div>
+      )
+    }
+  })
+
+  const oAuthProviders = Object.values(providers).map((provider) => {
+    if (provider.name === 'Email') {
+      return
+    }
+
+    return (
+      <div key={provider.name}>
+        <Button
+          type="button"
+          onClick={() => signIn(provider.id)}
+          text={`Sign in with ${provider.name}`}
+          optionalClassNames="w-full rounded-lg p-2 my-2"
+        />
+      </div>
+    )
+  })
 
   return (
     <>
@@ -22,7 +72,8 @@ export default function SignIn({
         <section className=" relative sm:w-11/12 md:w-[400px] max-w-[400px] flex flex-col items-center justify-center rounded-3xl p-6 md:p-8 bg-tertiaryGold">
           <SmallShopAlt className="text-primaryRed" height={125} width={125} />
           <h1 className="text-3xl pb-5">SIGN IN</h1>
-          <SignInProviders providers={providers} />
+          {emailProvider}
+          {oAuthProviders}
         </section>
       </Main>
     </>
