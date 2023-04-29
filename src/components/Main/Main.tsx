f784a9c29ea9e5f8cdf617b77b45b3ff3e435635
Matthew Powell <matt.powell2784@gmail.com>
2023-04-29 13:16:37 +0100

    main bg updated

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,15 +8,7 @@ interface HomePageProps {
 export const Main = ({ children }: HomePageProps) => {
   return (
     <main className="relative w-screen min-w-[280px] top-[3rem] md:top-[4rem]">
-      <div className="absolute w-full h-full -z-10 bg-tertiaryBlack ">
-        <Image
-          src="/curry_platter_bg.jpg"
-          fill
-          style={{ objectFit: 'cover', opacity: '0.4' }}
-          alt="Indian Platter"
-          quality={30}
-        />
-      </div>
+      <div className="absolute w-full h-full -z-10 bg-tertiaryBlack "></div>
       <section className="relative flex w-full min-h-screen flex-col items-center justify-start py-6 md:py-8">
         {children}
       </section>
