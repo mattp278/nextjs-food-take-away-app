defed1e4b39c6999602e9b3c5bb67460f15cfeaa
Matthew Powell <matt.powell2784@gmail.com>
2023-04-29 06:36:49 +0100

    brandon font added to app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,27 @@ import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
 import { makeStore } from '../redux/store/store'
 import { persistStore } from 'reduxjs-toolkit-persist'
 import { SessionProvider } from 'next-auth/react'
+import localFont from '@next/font/local'
 
 const store = makeStore()
 const persistor = persistStore(store)
+const brandonGrotFont = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Brandon_reg.otf',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/Brandon_bld.otf',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/Brandon_blk.otf',
+      weight: '900',
+    },
+  ],
+  variable: '--font-brandon',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   const { session } = pageProps
@@ -16,7 +34,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider session={session}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
+          <main className={brandonGrotFont.className}>
+            <Component {...pageProps} />
+          </main>
         </PersistGate>
       </Provider>
     </SessionProvider>
