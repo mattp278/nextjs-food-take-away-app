8470e594ccfa54cf98cd646bfbda6ad042bfc242
Matthew Powell <matt.powell2784@gmail.com>
2023-05-13 10:59:48 +0100

    add foodId route

--- src/pages/pages/food-menu/[...foodId].tsx
+++ src/pages/pages/food-menu/[...foodId].tsx
@@ -1,62 +1,58 @@
-// import { apiCall } from '@/utils/apiUtil'
-// import { Main, FoodPageItem, Navbar } from '@/components'
-// import { GetStaticPropsContext } from 'next'
-// import { TSFoodMenuItem } from '@/ts/interfaces'
+import { apiCall } from '@/utils/apiUtil'
+import { Main, FoodPageItem, Navbar } from '@/components'
+import { GetStaticPropsContext } from 'next'
+import { TSFoodMenuItem } from '@/ts/interfaces'
 
-// interface TSFoodItemResponse {
-//   foodItem: TSFoodMenuItem
-// }
+interface TSFoodItemResponse {
+  foodItem: TSFoodMenuItem
+}
 
-// export default function FoodItemPage(foodItem: TSFoodItemResponse) {
-//   const foodItemObj = foodItem.foodItem
+export default function FoodItemPage(foodItem: TSFoodItemResponse) {
+  const foodItemObj = foodItem.foodItem
 
-//   return (
-//     <>
-//       <title>Curry Club</title>
-//       <Navbar />
-//       <Main bgColorProp="bg-quaternaryGrey/25">
-//         {foodItem && <FoodPageItem foodItem={foodItemObj} />}
-//       </Main>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <title>Curry Club</title>
+      <Navbar />
+      <Main bgColorProp="bg-quaternaryGrey/25">
+        {foodItem && <FoodPageItem foodItem={foodItemObj} />}
+      </Main>
+    </>
+  )
+}
 
-// export async function getStaticPaths() {
-//   try {
-//     const foodItemRequest = await apiCall({
-//       httpMethod: 'GET',
-//       route: 'http://localhost:3000/api/v1/food-item/food-item',
-//     })
+export async function getStaticPaths() {
+  try {
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: 'http://localhost:3000/api/v1/food-item/food-item',
+    })
 
-//     const paths = foodItemRequest.data.map((foodItem: any) => {
-//       return { params: { foodId: [foodItem.id.toString()] } }
-//     })
+    const paths = foodItemRequest.data.map((foodItem: any) => {
+      return { params: { foodId: [foodItem.id.toString()] } }
+    })
 
-//     return { paths, fallback: 'blocking' }
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
+    return { paths, fallback: 'blocking' }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//   try {
-//     const foodId = context.params?.foodId
-//     const foodItemRequest = await apiCall({
-//       httpMethod: 'GET',
-//       route: `http://localhost:3000/api/v1/food-item/${foodId}`,
-//     })
+export async function getStaticProps(context: GetStaticPropsContext) {
+  try {
+    const foodId = context.params?.foodId
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: `http://localhost:3000/api/v1/food-item/${foodId}`,
+    })
 
-//     const foodItem = foodItemRequest.data.foodItem
+    const foodItem = foodItemRequest.data.foodItem
 
-//     return {
-//       props: { foodItem },
-//       revalidate: 60,
-//     }
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-
-export default function FoodItemPage() {
-  return <div>Food Item Page</div>
+    return {
+      props: { foodItem },
+      revalidate: 60,
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
