bce8df6800e90413125177beac52b2cb1a668959
Matthew Powell <matt.powell2784@gmail.com>
2023-05-16 16:41:36 +0100

    gets payment amount and runs payment intent

--- src/pages/api/stripe/stripe-payment-intent.ts
+++ src/pages/api/stripe/stripe-payment-intent.ts
@@ -1,32 +1,28 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 import Stripe from 'stripe'
+import { apiCall } from '@/utils/apiUtil'
 
-// This is your test secret API key.
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
   apiVersion: '2022-11-15',
 })
 
-const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-
-  return 1400
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { amount } = req.body
-  console.log('amount handler', amount)
-  console.log('calculateOrderAmount(amount),', calculateOrderAmount(amount))
+  const { orderId } = req.body
 
-  // Create a PaymentIntent with the order amount and currency
+  const order = await apiCall({
+    httpMethod: 'GET',
+    route: `api/v1/order/single-order/${orderId}`,
+  })
+
+  const totalPrice = +order.data.totalPrice * 100
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(amount),
-    currency: 'eur',
+    amount: totalPrice,
+    currency: 'GBP',
     automatic_payment_methods: {
       enabled: true,
     },
