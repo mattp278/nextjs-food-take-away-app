5bea5f6281944362d21f366d04f9fad419affd03
Matthew Powell <matt.powell2784@gmail.com>
2023-05-17 16:19:09 +0100

    chnage cors header to *

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'http://localhost:3000',
+            value: '*',
           },
           {
             key: 'Access-Control-Allow-Methods',
