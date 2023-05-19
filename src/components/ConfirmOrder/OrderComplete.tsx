a106aad8f28035aa948296d9ad843eae4a739921
Matthew Powell <matt.powell2784@gmail.com>
2023-05-19 16:12:50 +0100

    remove shadow for small screen sizes

--- src/components/ConfirmOrder/OrderComplete.tsx
+++ src/components/ConfirmOrder/OrderComplete.tsx
@@ -18,17 +18,17 @@ export const OrderComplete = () => {
   }, [pendingOrderId, dispatch])
 
   return (
-    <section className="flex flex-col justify-center items-center w-11/12 lg:w-1/2 rounded-3xl shadow-lg md:p-5 bg-quaternaryGrey m-8 ">
+    <section className="m-8 flex w-11/12 flex-col items-center justify-center rounded-3xl bg-quaternaryGrey md:p-5 md:shadow-lg lg:w-1/2 ">
       <DeliveryTruck className="text-primaryPink" height={125} width={125} />
-      <h1 className="text-3xl text-center p-2">ORDER CONFIRMED</h1>
-      <p className="text-xl mt-5">Your order number is:</p>
+      <h1 className="p-2 text-center text-3xl">ORDER CONFIRMED</h1>
+      <p className="mt-5 text-xl">Your order number is:</p>
       {confirmedOrderId ? (
-        <p className="text-secondaryWhite bg-primaryPink text-base md:text-xl p-3 mb-10">
+        <p className="mb-10 bg-primaryPink p-3 text-base text-secondaryWhite md:text-xl">
           {confirmedOrderId}
         </p>
       ) : null}
 
-      <div className="relative min-w-[15rem] min-h-[3.5rem] md:min-w-[20rem] md:min-h-[5rem] lg:ml-6 ">
+      <div className="relative min-h-[3.5rem] min-w-[15rem] md:min-h-[5rem] md:min-w-[20rem] lg:ml-6 ">
         <Image
           src="/curry_club_pink.png"
           fill
